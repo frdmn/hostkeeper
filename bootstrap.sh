70c5829e18ccb4a1f69cfa08f253999fd4685778
Jonas Friedmann <j@frd.mn>
2015-07-03 11:38:02 +0200

    Copy dnsmasq base configs from FS

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# dnsmasq configuration file
-dnsmasqconfig="$(cat <<-ENDOFCONFIG
-no-dhcp-interface=
-server=8.8.8.8
-
-no-hosts
-addn-hosts=/etc/dnsmasq.hosts
-ENDOFCONFIG
-)"
-
 # Parse debug arg from Ruby
 [[ $1 == "true" ]] && set -x
 
@@ -35,9 +25,9 @@ if [[ ! -n $(dpkg -l | grep dnsmasq) ]]; then
     apt-get install -y curl vim git build-essential                 # ... requirements
     apt-get install -y apache2 php5 libapache2-mod-php5 php5-mysql  # ... web server + php
     apt-get install -y dnsmasq                                      # ... dnsmasq
-    # Base config for dnsmasq
-    echo "${dnsmasqconfig}" > /etc/dnsmasq.conf
-    echo "1.2.3.4 this.is.a.tld.test" >> /etc/dnsmasq.hosts
+    # Apply base config for dnsmasq
+    cp /vagrant/opt/dnsmasq.conf /etc/dnsmasq.conf
+    cp /vagrant/opt/dnsmasq.hosts /etc/dnsmasq.hosts
     # Restart dnsmasq
     service dnsmasq restart
     # Final success message
