ae107f1b8d60eed07b91d75160061e1bf38343cf
Jonas Friedmann <j@frd.mn>
2015-07-05 18:11:38 +0200

    Add test routes for API server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,18 @@
 /* Require modules */
 
 var config = require('./config.json')
-    , jsonServer = require('json-server');
+    , jsonServer = require('json-server')
+    , http = require('http')
+    , Router = require('node-simple-router');
+
+/* Functions */
+
+// Function to construct dnsmasq hosts file based on json-server's database
+function updateHostsFile(callback){
+  var database = require('./' + config.database);
+
+  callback(database);
+}
 
 /* Logic */
 
@@ -17,8 +28,29 @@ function startJSONserver(){
   var router = jsonServer.router(config.database);
   server.use(router);
 
-  // Start json-server
+  // Start json-server on port 3000
   server.listen(3000);
 }
 
+// Function to start the actual API server
+function startAPIserver(){
+  // Create router instance
+  var router = new Router();
+
+  // Set routes
+  router.get('/hello', function(request, response) {
+    updateHostsFile(function(data){
+      response.end('data');
+      console.log(data);
+    });
+  });
+
+  // Create HTTP server
+  var server = http.createServer(router);
+
+  // Start server on port 4000
+  server.listen(4000);
+}
+
 startJSONserver();
+startAPIserver();
